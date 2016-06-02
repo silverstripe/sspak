4a153d15f63d8a0715433e35834f160434fd9977
Sam Minnee <sam@silverstripe.com>
2016-06-03 11:30:06 +1200

    NEW: CSV save/load commands for sspak
    This introduces 3 new commands:
    
     - loadcsv
     - savecsv
     - listtables
    
    The CSV commands will persist data as a folder of CSV files, 1 per
    table. This can be useful for transferring data across different
    database backends.

---
+++ tests/DataExtractor/CsvTableReaderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use SilverStripe\SsPak\DataExtractor\CsvTableReader;
+
+class CsvTableReaderTest extends PHPUnit_Framework_TestCase
+{
+	function testCsvReading() {
+
+		$csv = new CsvTableReader(__DIR__ . '/fixture/input.csv');
+		$this->assertEquals(['Col1', 'Col2', 'Col3'], $csv->getColumns());
+
+		$extractedData = [];
+		foreach($csv as $record) {
+			$extractedData[] = $record;
+		}
+
+		$this->assertEquals(
+			[
+				[ 'Col1' => 'One', 'Col2' => 2, 'Col3' => 'Three' ],
+				[ 'Col1' => 'Hello, Sam', 'Col2' => 5, 'Col3' => "Nice to meet you\nWhat is your name?" ]
+			],
+			$extractedData
+		);
+
+	}
+}
